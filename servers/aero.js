25ee82bb19949ab13add7c513bbe80f58156bce0
nodefish <mark.nader@gmail.com>
2017-06-17 20:39:31 +0400

    Create aero.js

---
+++ servers/aero.js
@@ -0,0 +1,27 @@
+global.Promise = require('bluebird');
+
+const AeroServer = require('aero/lib/Server');
+const server = new AeroServer();
+
+const headers = {
+        'Connection': 'close'
+};
+
+server.routes.GET = {
+	'': function(request, response) {
+		response.writeHead(200, headers);
+		response.end("Hello World!");
+	},
+
+	'keep-alive': function(request, response) {
+		response.end("Hello World!");
+	}
+};
+
+server.run({
+	config: {
+		ports: {
+			http: 8000
+		}
+	}
+});
